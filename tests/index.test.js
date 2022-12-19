7f2f37d44611b1b972af7ea3a72481aa2bcb75e8
Aleksandr Statciuk <github@oiya.xyz>
2022-12-19 21:46:48 +0300

    wip

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,14 +2,16 @@ import fs from 'fs'
 import path from 'path'
 import autocrop from '../src/index.js'
 
-it('can crop the image 1', async () => {
-  const image = fs.readFileSync(path.resolve(__dirname, './__data__/input/image_1.png'))
-  const base64 = Buffer.from(image).toString('base64')
-  const expected = await import('./__data__/expected/image_1').then(r => r.default)
+it('can crop the images', async () => {
+  for (let filename of ['image_1', 'image_2', 'image_3', 'image_4', 'image_5', 'image_6']) {
+    const image = fs.readFileSync(path.resolve(__dirname, `./__data__/input/${filename}.png`))
+    const base64 = Buffer.from(image).toString('base64')
+    const expected = await import(`./__data__/expected/${filename}`).then(r => r.default)
 
-  const result = await autocrop(`data:image/png;base64,${base64}`)
+    const result = await autocrop(`data:image/png;base64,${base64}`)
 
-  expect(result.data).toBe(expected.data)
-  expect(result.bbox).toMatchObject(expected.bbox)
-  expect(result.bgColor).toBe(expected.bgColor)
+    expect(result.data).toBe(expected.data)
+    expect(result.bbox).toMatchObject(expected.bbox)
+    expect(result.bgColor).toBe(expected.bgColor)
+  }
 })
