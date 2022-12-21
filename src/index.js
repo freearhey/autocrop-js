5c3bfd3d479f6d1c4970b19794a819cf4cda5443
Aleksandr Statciuk <github@oiya.xyz>
2022-12-22 00:39:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { Image } from 'image-js'
 import { getBgColor, calcBoundaryBox, rgbToHex } from './util.js'
 
-export default async function (dataURL, config = {}) {
+export default async function (input, config = {}) {
   config = { alphaThreshold: 0, ...config }
 
-  let _image = await Image.load(dataURL)
+  let _image = await Image.load(input)
   _image = new Image(_image.width, _image.height, _image.getRGBAData())
 
   const _bgColor = getBgColor(_image)
@@ -13,18 +13,18 @@ export default async function (dataURL, config = {}) {
 
   _image = _image.crop({ x, y, width, height })
 
-  const base64 = _image.toBase64('image/png')
-  const data = `data:image/png;base64,${base64}`
   const bgColor = rgbToHex(_bgColor)
+  const base64 = _image.toBase64('image/png')
+  const dataURL = `data:image/png;base64,${base64}`
 
   return {
-    data,
     bbox: {
       x,
       y,
       width,
       height
     },
-    bgColor
+    bgColor,
+    dataURL
   }
 }
