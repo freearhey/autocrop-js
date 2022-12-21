ce718013e6d9fb996e15236aefc58c75a9d7e80d
Aleksandr Statciuk <github@oiya.xyz>
2022-12-21 04:55:06 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import { Image } from 'image-js'
 import { calcBoundaryBox, getBgColor, removeBg, rgbToHex, getPixels, hexToRgb } from './util.js'
 
-export default async function (dataURL, options = {}) {
+export default async function (dataURL, config = {}) {
+  config = { threshold: 0, alphaThreshold: 0, backgroundColor: null, ...config }
+
   let _image = await Image.load(dataURL)
   _image = new Image(_image.width, _image.height, _image.getRGBAData())
-  // console.log(_image.width, _image.height)
 
-  const bgColorRGB = getBgColor(_image)
+  const _bgColor = config.backgroundColor ? hexToRgb(config.backgroundColor) : getBgColor(_image)
+  console.log(_bgColor)
 
-  const [x, y, width, height] = calcBoundaryBox(_image, bgColorRGB)
+  const [x, y, width, height] = calcBoundaryBox(
+    _image,
+    _bgColor,
+    config.threshold,
+    config.alphaThreshold
+  )
+
   _image = _image.crop({ x, y, width, height })
-  // console.log(x, y, width, height)
 
   const base64 = _image.toBase64('image/png')
-  const bgColorHEX = rgbToHex(bgColorRGB)
+  const data = `data:image/png;base64,${base64}`
+  const bgColor = rgbToHex(_bgColor)
 
   return {
-    data: `data:image/png;base64,${base64}`,
+    data,
     bbox: {
       x,
       y,
       width,
       height
     },
-    bgColor: bgColorHEX
+    bgColor
   }
 }
--- src/util.js
+++ src/util.js
@@ -1,13 +1,5 @@
 import _ from 'lodash'
 
-export function getBgColor(image) {
-  return image.getPixel(0)
-}
-
-export function getPixels(image) {
-  return _.chunk(image.getRGBAData(), 4)
-}
-
 function calcMaxColor(color, threshold = 0, alphaThreshold = 0) {
   if (color[3] <= alphaThreshold) return [0, 0, 0, 0]
 
@@ -25,15 +17,15 @@ function removeBg(pixel, bgColor) {
   return JSON.stringify(pixel) === JSON.stringify(bgColor) ? [0, 0, 0, 0] : pixel
 }
 
-export function calcBoundaryBox(image, bgColor) {
+export function calcBoundaryBox(image, bgColor, threshold, alphaThreshold) {
   let pixels = getPixels(image)
   let width = image.width
   let height = image.height
 
-  let maxColor = calcMaxColor(bgColor)
+  let maxColor = calcMaxColor(bgColor, threshold, alphaThreshold)
 
   pixels = pixels.map(p => {
-    p = calcMaxColor(p)
+    p = calcMaxColor(p, threshold, alphaThreshold)
     p = removeBg(p, maxColor)
 
     return p
@@ -110,48 +102,12 @@ export function calcBoundaryBox(image, bgColor) {
   return [x1, y1, newWidth, newHeight]
 }
 
-function deltaE(rgbA, rgbB) {
-  let labA = rgb2lab(rgbA)
-  let labB = rgb2lab(rgbB)
-  let deltaL = labA[0] - labB[0]
-  let deltaA = labA[1] - labB[1]
-  let deltaB = labA[2] - labB[2]
-  let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2])
-  let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2])
-  let deltaC = c1 - c2
-  let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC
-  deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH)
-  let sc = 1.0 + 0.045 * c1
-  let sh = 1.0 + 0.015 * c1
-  let deltaLKlsl = deltaL / 1.0
-  let deltaCkcsc = deltaC / sc
-  let deltaHkhsh = deltaH / sh
-  let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh
-  return i < 0 ? 0 : Math.sqrt(i)
+export function getBgColor(image) {
+  return image.getPixel(0)
 }
 
-function rgb2lab(rgb) {
-  let r = rgb[0] / 255,
-    g = rgb[1] / 255,
-    b = rgb[2] / 255,
-    x,
-    y,
-    z
-  r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92
-  g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92
-  b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92
-  x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047
-  y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.0
-  z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883
-  x = x > 0.008856 ? Math.pow(x, 1 / 3) : 7.787 * x + 16 / 116
-  y = y > 0.008856 ? Math.pow(y, 1 / 3) : 7.787 * y + 16 / 116
-  z = z > 0.008856 ? Math.pow(z, 1 / 3) : 7.787 * z + 16 / 116
-  return [116 * y - 16, 500 * (x - y), 200 * (y - z)]
-}
-
-function componentToHex(c) {
-  var hex = c.toString(16)
-  return hex.length == 1 ? '0' + hex : hex
+export function getPixels(image) {
+  return _.chunk(image.getRGBAData(), 4)
 }
 
 export function rgbToHex(color) {
@@ -163,10 +119,11 @@ export function rgbToHex(color) {
 export function hexToRgb(hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-      }
+    ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16), 255]
     : null
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16)
+  return hex.length == 1 ? '0' + hex : hex
 }
